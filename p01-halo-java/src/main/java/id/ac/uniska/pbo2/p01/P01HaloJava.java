870b98cfaf29317a2dd2fd0fb8ca2478a4b089d7
putrisalsabila <slsptsls.10@gmail.com>
2026-09-20 12:06:02 +0800

    P01: proyek pertama HaloPBO2

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/P01HaloJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package id.ac.uniska.pbo2.p01;
+
+/**
+ *
+ * @author USER
+ */
+public class P01HaloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
