64dc925eae29e7d65eceb292deefa13528932197
putrisalsabila <slsptsls.10@gmail.com>
2026-09-20 12:29:34 +0800

    P01: tambah KartuMahasiswa

---
+++ p01-halo-java/src/main/java/id/ac/uniska/pbo2/p01/KartuMahasiswa.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.ac.uniska.pbo2.p01;
+
+
+/**
+ *
+ * @author USER
+ */
+public class KartuMahasiswa {
+
+        public static void main(String[] args) {
+
+        String nama = "Putri Sal Sabila";
+        String npm = "2410010086";
+        String prodi = "Teknik Informatika";
+        int semester = 5;
+        String alasan = "Ingin lebih paham cara kerja pemrograman Java";
+
+        System.out.println("==============================");
+        System.out.println("     KARTU MAHASISWA PBO 2");
+        System.out.println("==============================");
+        System.out.println("Nama     : " + nama);
+        System.out.println("NPM      : " + npm);
+        System.out.println("Prodi    : " + prodi);
+        System.out.println("Semester : " + semester);
+        System.out.println("Alasan   : " + alasan);
+        System.out.println("==============================");
+    }
+}
